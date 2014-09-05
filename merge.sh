b0e97f2e31e3b9d1b83f5fd967cd16adf6e2e00a
Mark Wotton <mwotton@gmail.com>
2014-09-05 19:21:49 +0700

    merge algo

---
+++ merge.sh
@@ -0,0 +1,7 @@
+
+# http://www.sqlite.org/lang_attach.html
+# basic plan: copy the first one to a new location
+# attach each other one in turn, insert into frontpage_current select
+# * from attoched
+
+# for i in
